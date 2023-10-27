b85d126f563cbdcf14609aa4dd5a632d4e62d12f
Christian Rodríguez Lara <christianrodriguezlara6@gmail.com>
2023-10-27 16:00:23 +0200

    Ejercicios 7 y 8 Tema 2

---
+++ 02_Variables/ejercicios/T02_Ejercicio07.java
@@ -0,0 +1,15 @@
+/**
+ * Explica tu código aquí
+ * 
+ * @author Christian Rodríguez Lara
+ */
+public class T02_Ejercicio07 {
+  public static void main(String[] args) {
+    char primera_letra = 'a';
+    char ultima_letra = 'z';
+    String palabra = "abecedario";
+
+    System.out.println(primera_letra + " " + ultima_letra + palabra);
+
+  }
+}
---
+++ 02_Variables/ejercicios/T02_Ejercicio08.java
@@ -0,0 +1,18 @@
+/**
+ * Explica tu código aquí
+ * 
+ * @author Christian Rodríguez Lara
+ */
+public class T02_Ejercicio08 {
+  public static void main(String[] args) {
+    char letra1 = 'H';
+    char letra2 = 'o';
+    char letra3 = 'l';
+    char letra4 = 'a';
+    char letra5 = '!';
+
+    String palabra = "" + letra1 + letra2 + letra3 +letra4 + letra5;
+    
+    System.out.println(palabra);
+  }
+}
